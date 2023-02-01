d4e78c126d3f9461a0f62f0ad9205397edae599b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-02-01 10:18:44 +0100

    Disable readonly filesystem

--- bin/run-tests-in-docker.sh
+++ bin/run-tests-in-docker.sh
@@ -18,13 +18,14 @@ set -e
 # Build the Docker image
 docker build --rm -t exercism/chapel-test-runner .
 
+# TODO: enable --read-only flag
+
 # Run the Docker image using the settings mimicking the production environment
 docker run \
     --rm \
     --network none \
-    --read-only \
     --mount type=bind,src="${PWD}/tests",dst=/opt/test-runner/tests \
-    --mount type=tmpfs,dst=/tmp \
+    --mount type=volume,dst=/tmp \
     --volume "${PWD}/bin/run-tests.sh:/opt/test-runner/bin/run-tests.sh" \
     --workdir /opt/test-runner \
     --entrypoint /opt/test-runner/bin/run-tests.sh \
